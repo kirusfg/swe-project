166e03fcdc916b0cc7d5fd4a5e89d3b10871dee7
Kirill Kirillov <kirusfg@gmail.com>
2021-12-06 23:16:32 +0500

    Make the phone numbers unique

--- src/main/java/kz/edu/nu/hotel/model/PhoneNumber.java
+++ src/main/java/kz/edu/nu/hotel/model/PhoneNumber.java
@@ -9,7 +9,7 @@ public class PhoneNumber {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, length = 100)
+    @Column(nullable = false, unique = true, length = 100)
     private String number;
 
     protected PhoneNumber() {}
